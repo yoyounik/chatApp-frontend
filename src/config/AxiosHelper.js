1b16c590970bab16790f1ef706736445c1e645dc
yoyounik <sinhanikhil909@gmail.com>
2025-08-19 22:45:47 +0530

    changes

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL = "http://localhost:8080";
+export const baseURL = "https://chatapp-backend-67bn.onrender.com";
 export const httpClient = axios.create({
   baseURL: baseURL,
 });
